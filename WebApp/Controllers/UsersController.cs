9ff8c4259eda2ad4e23039d5686490e1fb430032
Liron Cohen <liron@flame-ware.com>
2021-02-15 16:32:21 +0200

    WebApp-Fixed Routes from UsersController

--- WebApp/Controllers/UsersController.cs
+++ WebApp/Controllers/UsersController.cs
@@ -3,6 +3,7 @@ using Entities;
 using UsersApp.Services;
 
 namespace UsersApp.Controllers {
+		[Route("users")]
 	public class UsersController : Controller {
 		private IUsersService service;
 
@@ -10,27 +11,30 @@ namespace UsersApp.Controllers {
 			this.service = service;
 		}
 
+		[HttpGet]
 		public IActionResult Index() {
 			return View(service.GetAll());
 		}
 
+		[HttpGet("{id}")]
 		public IActionResult Details(int id) {
 			var model = service.GetById(id);
 			if(model == null)
 				return NotFound();
 			return Ok(model);
 		}
-		[HttpPost]
+		[HttpPost("{id}")]
 		public IActionResult Save(User model) {
 			service.Update(model);
 			return Ok();
 		}
 
+		[HttpGet("new")]
 		public IActionResult Add() {
 			var item = new User();
 			return View(item);
 		}
-		[HttpPost]
+		[HttpPost("new")]
 		public IActionResult Add(User model) {
 			service.Add(model);
 			return RedirectToAction("Index");
