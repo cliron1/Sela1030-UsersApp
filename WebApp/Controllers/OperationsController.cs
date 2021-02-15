c8cf8f18ac785e64d4c1334a3855f8990f9ea9e5
Liron Cohen <liron@flame-ware.com>
2021-02-15 14:56:20 +0200

    explained DI

---
+++ WebApp/Controllers/OperationsController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApp.DI;
+
+namespace WebApp.Controllers {
+	[Route("ops")]
+	public class OperationsController : Controller {
+		private readonly IOperationTransient tr;
+		private readonly IOperationScoped sc;
+		private readonly IOperationSingleton sn;
+
+		public OperationsController(
+				IOperationTransient tr,
+				IOperationScoped sc,
+				IOperationSingleton sn
+			) {
+			this.tr = tr;
+			this.sc = sc;
+			this.sn = sn;
+		}
+
+		[HttpGet]
+		public IActionResult Index() {
+			var result = new {
+
+				fromMiddleware = HttpContext.Items["DI-Tests"],
+
+				fromController = new {
+					Transient = tr.Uid,
+					Scoped = sc.Uid,
+					Singleton = sn.Uid
+				}
+			};
+			
+			return Ok(result);
+		}
+	}
+}
---
+++ WebApp/DI/Operation.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace WebApp.DI {
+	public interface IOperation {
+		string Uid { get; }
+	}
+
+	public interface IOperationTransient : IOperation {
+	}
+	public interface IOperationScoped : IOperation {
+	}
+	public interface IOperationSingleton : IOperation {
+	}
+
+	public class Operation : IOperationTransient, IOperationScoped, IOperationSingleton {
+		public string Uid { get; } = Guid.NewGuid().ToString()[^4..];
+	}
+}
