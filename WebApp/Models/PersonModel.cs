fcaea75ab7464ff53cf328a4e0b9de5093eb4e26
Liron Cohen <liron@flame-ware.com>
2021-03-04 00:05:33 +0200

    EditorForModel + Html.BeginForm

--- WebApp/Models/PersonModel.cs
+++ WebApp/Models/PersonModel.cs
@@ -1,6 +1,12 @@
-﻿namespace UsersApp.Models {
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UsersApp.Models {
 	public class Person {
+		[Required(ErrorMessage = "Mandatory field")]
+		[StringLength(20, ErrorMessage = "Maximum 20 chars")]
 		public string Name { get; set; }
+
+		[Range(1, 120, ErrorMessage = "A person can be 1 to 120 years old")]
 		public int Age { get; set; }
 
 		public bool IsAdult() {
